ef83689d60ea32b09b607dfed8e48a80600695d5
Michelle S <MichelleShaheen@cmail.carleton.ca>
2018-10-09 12:16:05 +0500

    Add files via upload

--- Test/CardsPlayTest/CardTest.java
+++ Test/CardsPlayTest/CardTest.java
@@ -1,1 +1,18 @@
-#empty
+package CardsPlay;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CardTest {
+    
+    @Test
+    public void test(){
+        Card card = new Card(1);//first card
+        String value = card.getCard();
+        int value1 = Integer.valueOf(value);
+        int rank = card.getRank();
+        int suit = card.getSuit();
+        int output = rank + suit;
+        assertEquals(value1, output);//checks first cards with first choosen
+    }
+}
