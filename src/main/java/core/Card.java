171473a5bc5a424da4120aa4b3f5903668dfd045
Michelle S <MichelleShaheen@cmail.carleton.ca>
2018-10-09 11:50:34 +0500

    Add files via upload

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -1,61 +1,34 @@
-package core;
+package CardsPlay;
 
-public class Card implements Comparable<Card>
-{ 
-		private Suit suit;
-		private Rank rank;
-		
-		public Card(Rank rank, Suit suit){
-			this.suit = suit;
-			this.rank = rank;
-		}
-		
-		public Suit getSuit() {
-			return suit;
-		}
+/**
+ * @version 2.1.1
+ */
+public class Card {
 
-		public void setSuit(Suit suit) {
-			this.suit = suit;
-		}
+    private static final String[] SUITS = {"\u2660", "\u2665", "\u2666", "\u2663"};//ASCII codes for heart spades diamond clubs
+    private static final String[] RANKS = {"2", "3", "4", "5", "6", "7", "8", "9", "10",
+         "J", "Q", "K", "A"};
+    private final int cardNum; // the number of a card (1-52)
 
-		public Rank getRank() {
-			return rank;
-		}
-
-		public void setRank(Rank rank) {
-			this.rank = rank;
-		}
-		
-		public String toString(){
-			return suit + " " + rank;
-		}
-
-		public int Equals(Card c) 
-		{
-		     if (this.rank == (c.rank))
-		            return 0;
-		     else if ((this.rank) > (c.rank))
-		            return 1;
-		     else
-		           return -1;
-		}
-		
-		public boolean isSuit(int suit){
-			if(suit >= 1 && suit <= 4)
-				return true;
-			return false;
-		}
-		
-		public boolean isRank(int rank){
-			if(rank >= 1 && rank <= 13)
-				return true;
-			return false;
-		}
-
-		public int compareTo(Card c) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
+    /**
+     * constructor for card class
+     * @param c - card number (1-52 for standard 52-deck) initialize to
+     */
+    public Card(int c) {
+        cardNum = c;
+    }
 
 
-	}
+    
+    public int getRank() {
+        return cardNum % 13; // assuming 52-card deck, 13 of each suit
+    }
+
+    public int getSuit() {
+        return cardNum % 4; // assuming 52-card deck, 4 of each rank
+    }
+
+    public String getCard() {
+        return RANKS[getRank()] + SUITS[getSuit()];
+    }
+}
--- src/main/java/core/Deck.java
+++ src/main/java/core/Deck.java
@@ -1,27 +1,26 @@
-package core;
+package CardsPlay;
 
-import java.util.Random;
-import java.util.ArrayList;
+/**
+ * @version 2.1.1
+ * Deck interface for Poker.
+ */
+public interface Deck {
 
-public class Deck {
-	private Card cards[];
-	public Deck(){
-		this.cards = new Card[52];
-		for (int i = 0; i < 52; i++) {
-			// creates the card
-			Card card = new Card(null, null);
-			// adds card to the deck
-			this.cards[i] = card; 
-		}
-	}
+    /**
+     * method to initially set up a deck
+     * size, etc
+     */
+    public void initializeDeck();
+
+    //for shuffling
+    public void shuffleDeck();
+
+    //return deck size
+    public int getDeckSize();
+    
+    public void getInitializable();
+
+    //return card object
+    Card draw();
+
 }
-
-
-/* Creating all possible cards... 
-for (Suits s : Suits.values()) {
-    for (Ranks r : Ranks.values()) {
-         Card c = new Card(s,r);
-    }  
-}
-using enum for card class
-*/
